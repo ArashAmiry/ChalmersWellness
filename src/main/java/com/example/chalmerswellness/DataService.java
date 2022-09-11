3035eb4f175cf20639beac1e20861838c3d0187f
Gustav Berndtzen <gustav.berndtzen@gmail.com>
2022-09-11 11:45:31 +0200

    [fix] now creates filepath to database

--- src/main/java/com/example/chalmerswellness/DataService.java
+++ src/main/java/com/example/chalmerswellness/DataService.java
@@ -1,16 +1,23 @@
 package com.example.chalmerswellness;
 
+import java.io.File;
 import java.sql.*;
 
 public class DataService {
+    private String dbPath = "C:\\sqlite\\db";
 
     public DataService(){
         createNewDatabase("testDb");
         createNewTable();
     }
 
-    public static void createNewDatabase(String fileName) {
-        String url = "jdbc:sqlite:C:/sqlite/db/" + fileName;
+    public void createNewDatabase(String fileName) {
+        File sqliteFolder = new File(dbPath);
+        if(!sqliteFolder.exists())
+            sqliteFolder.mkdirs();
+
+        dbPath += "/" + fileName;
+        String url = "jdbc:sqlite:" + dbPath;
 
         try {
             Class.forName("org.sqlite.JDBC");
@@ -20,14 +27,12 @@ public class DataService {
                 System.out.println("The driver name is " + meta.getDriverName());
                 System.out.println("A new database has been created.");
             }
-
         } catch (SQLException | ClassNotFoundException e) {
             System.out.println(e.getMessage());
         }
     }
 
     public static void createNewTable() {
-        //TODO make dyanmic
         String url = "jdbc:sqlite:C://sqlite/db/testDb.db";
 
         String sql = "CREATE TABLE IF NOT EXISTS warehouses (\n"
