675214ca9d74fb7e729cec5a85ebef03c93d9aa0
Arash Amiry <ArashAmiry@hotmail.com>
2022-09-21 23:05:53 +0200

    [Update] food assertion

--- src/main/java/com/example/chalmerswellness/calorieAPI/FoodFacade.java
+++ src/main/java/com/example/chalmerswellness/calorieAPI/FoodFacade.java
@@ -2,31 +2,23 @@ package com.example.chalmerswellness.calorieAPI;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 public class FoodFacade {
 
-    public Food createNutritionModelFor(String query){
-        NutritionAPIConnector apiConnector = new NutritionAPIConnector();
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.registerTypeAdapter(Food.class, new NutritionTypeAdapter());
-
-        gsonBuilder.setPrettyPrinting();
-        Gson gson = gsonBuilder.create();
-
-        String nutritionJsonString = apiConnector.getNutritionAsStringFromAPI(query);
-        Food[] nutrition = gson.fromJson(nutritionJsonString, Food[].class);
-
-        return nutrition[0];
-    }
-
-    public Food createFood(String food) throws JsonProcessingException {
+    public Food createFood(String foodName) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         NutritionAPIConnector apiConnector = new NutritionAPIConnector();
-        String nutritionJsonString = apiConnector.getNutritionAsStringFromAPI(food);
+        String nutritionJsonString = apiConnector.getNutritionAsStringFromAPI(foodName);
 
-        Food[] foodModel = objectMapper.readValue(nutritionJsonString, Food[].class);
-        return foodModel[0];
+        Food[] food = objectMapper.readValue(nutritionJsonString, Food[].class);
+
+        assertFoodExistence(food);
+        return food[0];
+    }
+
+    private void assertFoodExistence(Food[] food) {
+        if (food.length == 0) {
+            throw new RuntimeException("No food with that name exists in our database.");
+        }
     }
 }
