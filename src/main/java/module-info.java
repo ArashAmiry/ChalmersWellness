c28fc744cae526741d43efc9bd0568d983f9beb3
Gustav Berndtzen <gustav.berndtzen@gmail.com>
2022-09-23 22:30:00 +0200

    [add] maven dependency to project snapshot

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,4 @@
 module com.example.chalmerswellness {
-    requires javafx.controls;
-    requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires java.sql;
@@ -9,6 +6,7 @@ module com.example.chalmerswellness {
     requires com.fasterxml.jackson.databind;
     requires org.json;
     requires com.google.gson;
+    requires javafx.fxml;
 
     opens com.example.chalmerswellness to javafx.fxml;
     exports com.example.chalmerswellness;
