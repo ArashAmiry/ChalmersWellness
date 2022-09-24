af2f12a0ff0347d0d62e0bec1c1ae160f9d5845e
Svanstedt <nils.svanstedt@gmail.com>
2022-09-24 22:28:17 +0200

    [Fix] Fixed small bugs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,12 +9,9 @@ module com.example.chalmerswellness {
     requires com.google.gson;
     requires com.fasterxml.jackson.databind;
     requires org.json;
-    requires com.google.gson;
 
     opens com.example.chalmerswellness to javafx.fxml;
     exports com.example.chalmerswellness;
-    exports com.example.chalmerswellness.Controllers;
-    opens com.example.chalmerswellness.Controllers to javafx.fxml;
     exports com.example.chalmerswellness.Models;
     opens com.example.chalmerswellness.Models to javafx.fxml;
     exports com.example.chalmerswellness.ObjectModels;
