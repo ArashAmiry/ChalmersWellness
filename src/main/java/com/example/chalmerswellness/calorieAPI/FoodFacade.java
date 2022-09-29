c595e53ba4b08a2826ab1741428f02f868fac110
Arash Amiry <ArashAmiry@hotmail.com>
2022-09-29 21:35:26 +0200

    [Merge conflict] Gustav accidently changes Dataservice class into workoutservice

--- src/main/java/com/example/chalmerswellness/calorieAPI/FoodFacade.java
+++ src/main/java/com/example/chalmerswellness/calorieAPI/FoodFacade.java
@@ -2,6 +2,7 @@ package com.example.chalmerswellness.calorieAPI;
 
 import com.example.chalmerswellness.Interfaces.Observable;
 import com.example.chalmerswellness.Interfaces.Observer;
+import com.example.chalmerswellness.Services.DataService;
 import com.example.chalmerswellness.Services.DatabaseConnector;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
